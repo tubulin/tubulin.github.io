d484c28ad6cde441d4fa2d2076621a7df58c1b30
tubulin <32159561+tubulin@users.noreply.github.com>
2017-09-21 20:33:49 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include("yla.php"); ?>
+        		<h1>tubulin.github.io</h1>
+
+				<p>Kesällä 2017 opettelin PHP-ohjelmoinnin perusteita ja innostuin Ohjelmointiputkan PHP-haasteesta. Kesän aikana ratkoin tehtävistä 71/100. PHP-haasteesta voi lukea lisää <a href="https://www.ohjelmointiputka.net/phph/tietoa.php">täältä</a>. Tässä pari esimerkkiä ratkaisuistani:</p>
+    		<ul>
+
+        		<li><a href="  ">Rot13-käännös</a></li>
+
+        		<li><a href="  ">Neliön tulostus kirjaimilla</a></li>
+
+			</ul>
+
+<p>Ollessani vaihdossa Itävallassa, teimme yhdellä geoinformatiikan kurssilla SQL-tietokannan Wienin nähtävyyksistä ja esitimme tiedot google maps -kartalla hyödyntyen PHP:tä. Tein suurin piirtein saman uudelleen hyödyntäen Vantaan nähtävyyksiä:</p>
+    		<ul>
+
+        		<li><a href="  ">Versio 1: kaikki nähtävyydet kartalla</a></li>
+
+        		<li><a href="  ">Versio 2: kartallä näytettävät nähtävyydet voi valita tyypin mukaan</a></li>
+
+			</ul>
+<br>
+<?php include("ala.php"); ?>
